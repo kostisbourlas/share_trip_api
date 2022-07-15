6f51d9f22d6db3a8e74535cfd915004371b00005
Kostis Bourlas <kostisbourlas@protonmail.com>
2022-07-15 20:53:07 +0300

    Add hardcoded traveler objects

--- src/main/kotlin/com/sharethetrip/models/Traveler.kt
+++ src/main/kotlin/com/sharethetrip/models/Traveler.kt
@@ -3,4 +3,15 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Traveler(val id: String, val firstName: String, val lastName: String)
 
-val travelerStorage = mutableListOf<Traveler>()
+val travelerStorage = mutableListOf<Traveler>(
+    Traveler(
+        id = "1",
+        firstName = "Joe",
+        lastName = "Doe"
+    ),
+    Traveler(
+        id = "2",
+        firstName = "John",
+        lastName = "Smith"
+    ),
+)
