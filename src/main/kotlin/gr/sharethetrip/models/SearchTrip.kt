a92e74372bde6aec22031653d0500cf60a89a48d
Kostis Bourlas <kostisbourlas@protonmail.com>
2022-07-24 11:34:19 +0300

    Add test for searching trips

--- src/main/kotlin/gr/sharethetrip/models/SearchTrip.kt
+++ src/main/kotlin/gr/sharethetrip/models/SearchTrip.kt
@@ -5,3 +5,9 @@ data class SearchTrip(
     val addressFrom: String? = null,
     val addressTo: String? = null,
 )
+
+object SearchTripDao {
+    fun createSearchTripObject(addressFrom: String?, addressTo: String?): SearchTrip {
+        return SearchTrip(addressFrom, addressTo)
+    }
+}
---
+++ src/test/kotlin/gr/sharethetrip/SearcTripRoutesTest.kt
@@ -0,0 +1,35 @@
+package gr.sharethetrip
+
+import io.ktor.client.call.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.testing.*
+import kotlin.test.*
+
+
+class SearchTripRoutesTest {
+    @Test
+    fun searchTripsByDepartureAndArrivalAddresses() = testApplication {
+        val client = createClient {
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+
+        val response = client.get("search/trips") {
+            contentType(ContentType.Application.Json)
+            setBody(SearchTripDao.createSearchTripObject("Athens", "Pilio"))
+        }
+
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals(
+            """
+                [{"id":"1","driverId":"1","departureAddress":"Athens","arrivalAddress":"Pilio","departureDatetime":"15/08/2022 12:00","availableSeats":4,"description":null}]
+            """.trimIndent(),
+            response.bodyAsText()
+        )
+    }
+}
